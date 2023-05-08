7bb72f8ff030b066470668ce57f30c5c7825cf13
anhphuongdo <anhdo.31201024387@st.ueh.edu.vn>
2023-05-08 11:50:30 +0700

    hehe

--- DBBL/Models/CongTy.cs
+++ DBBL/Models/CongTy.cs
@@ -7,14 +7,9 @@ namespace DBBL.Models;
 public partial class CongTy
 {
     public string IdCt { get; set; } = null!;
-    [Display(Name = "Tên Công Ty")]
     public string TenCt { get; set; } = null!;
-    [Display(Name = "Họ Tên")]
     public string TenNql { get; set; } = null!;
-    [Display(Name = "Số Điện Thoại")]
     public string SđtNql { get; set; } = null!;
-    [Display(Name = "Email")]
     public string EmailNql { get; set; } = null!;
-    [Display(Name = "Mật Khẩu")]
     public string Mk { get; set; } = null!;
 }
--- DBBL/Models/SanPham.cs
+++ DBBL/Models/SanPham.cs
@@ -10,24 +10,12 @@ public partial class SanPham
 
     public string IdCt { get; set; } = null!;
 
-    [Display(Name = "Tên Sản Phẩm")]
     public string TenSp { get; set; } = null!;
-    [Display(Name = "Số Lượng Hiện Tại")]
-    [MinLength(0,ErrorMessage = "Giá trị Không được âm")]
+
     public string SlHt { get; set; } = null!;
-    [Display(Name = "Giới Hạn Số Lượng")]
-    [MinLength(0, ErrorMessage = "Giá trị Không được âm")]
+
     public string GioiHan { get; set; } = null!;
 
     public bool? Trangthai { get; set; }
 
-    public SanPham()
-    {
-        IdSp = "";
-        IdCt = "";
-        TenSp = "";
-        SlHt = "";
-        GioiHan = "";
-        Trangthai = true;
-    }
 }
